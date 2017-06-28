0095afdd2237dc69d837b032be5b24d5af8b0973
Christian Wiedemann <christian.wiedemann@key-tec.de>
2017-06-28 23:59:58 +0200

    Fix acp-docksal wrapper

--- bin/acp-docksal.sh
+++ bin/acp-docksal.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ANDOCK_CI_PROJECT_NAME=basename "$PWD"
+ANDOCK_CI_PROJECT_NAME=$(basename "$PWD")
 
-fin exec 'if test -e "/user/local/bin/acp";then
+fin exec "if test -e \"/user/local/bin/acp\";then
  curl -fsSL https://raw.githubusercontent.com/andock-ci/pipeline/master/install-pipeline | sh
 fi
 export ANDOCK_CI_INSIDE_DOCKSAL=true
-export ANDOCK_CI_PROJECT_NAME=$ANDOCK_CI_PROJECT_NAME
-acp $@'
+export ANDOCK_CI_PROJECT_NAME=${ANDOCK_CI_PROJECT_NAME}
+acp $@"
