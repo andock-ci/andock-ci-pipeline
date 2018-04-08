140dd9da788c03f22e12f4750d7315c22a8a4b23
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-04-08 23:05:05 +0200

    Add doctl

--- before_install.sh
+++ before_install.sh
@@ -11,3 +11,9 @@ ssh-add ~/.ssh/id_rsa
 mv -fv ssh-config ~/.ssh/config
 git config --global user.email "christian.wiedemann@key-tec.de"
 git config --global user.name "KEY-TEC (via TravisCI)"
+
+curl -sL https://github.com/digitalocean/doctl/releases/download/v1.7.2/doctl-1.7.2-linux-amd64.tar.gz | tar -xzv
+sudo mv ~/doctl /usr/local/bin
+doctl auth init -t $do_token
+
+doctl compute droplet-action restore 88798812 --image-id=33288941 --wait
