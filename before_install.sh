7f4f95692f7deb5cb2be1e4bf6190d97a52eded5
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-04-09 00:27:16 +0200

    Fix tests

--- before_install.sh
+++ before_install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+# Add encrypted ssh keys.
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
@@ -10,11 +11,15 @@ eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa
 mv -fv ssh-config ~/.ssh/config
 
-git config --global user.email "christian.wiedemann@key-tec.de"
-git config --global user.name "KEY-TEC (via TravisCI)"
-
+# Test against an digital ocean image.
+# Install doctl.
 curl -sL https://github.com/digitalocean/doctl/releases/download/v1.7.2/doctl-1.7.2-linux-amd64.tar.gz | tar -xzv
 sudo mv doctl /usr/local/bin
 doctl auth init -t $do_token
 
+# Test against an empty image.
+# Restore from snapshot.
 doctl compute droplet-action restore 88798812 --image-id=33288941 --wait
+
+# Sleep while image is rebooting.
+sleep 60
