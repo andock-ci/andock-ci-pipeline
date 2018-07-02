e344697fd16c5067b874cfa3fecbf6b532e831c2
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-07-02 14:49:03 +0200

    Rename andock-ci to andock_ci in roles.

--- bin/acp.sh
+++ bin/acp.sh
@@ -279,8 +279,8 @@ install_configuration ()
     generate_playbooks
     echo-green "Installing roles:"
     ansible-galaxy install andock_ci.build,v${REQUIREMENTS_ANDOCK_CI_BUILD} --force
-    ansible-galaxy install andock-ci.fin,v${REQUIREMENTS_ANDOCK_CI_FIN} --force
-    ansible-galaxy install andock-ci.server,v${REQUIREMENTS_ANDOCK_CI_SERVER} --force
+    ansible-galaxy install andock_ci.fin,v${REQUIREMENTS_ANDOCK_CI_FIN} --force
+    ansible-galaxy install andock_ci.server,v${REQUIREMENTS_ANDOCK_CI_SERVER} --force
     ansible-galaxy install j0lly.ssh-keys,v${REQUIREMENTS_SSH_KEYS} --force
     echo "
 [andock-ci-build-server]
